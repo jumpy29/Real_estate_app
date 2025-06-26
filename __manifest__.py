3e9da9e4228998b0b46fa34c3cbf913aaa4ebd03
Smit Amin <smitamin@uvic.ca>
2025-06-26 13:25:57 +0530

    initial commit - has initial files

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'estate', 
+    'version': '1.0', 
+    'description': 'a real estate app that lists properties, and allows buyers to place bids on them',
+    'depends': [
+        'base'
+    ],
+    'data': [
+        'security/ir.model.access.csv'
+    ],
+    'application': True
+
+}
---
+++ models/estate_properties.py
@@ -0,0 +1,20 @@
+from odoo import models, fields
+
+class EstateProperties(models.Model):
+    _name = 'estate.properties'  #name of table
+    _description = 'Real estate app'
+
+    #table fields
+    name = fields.Char(required=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
