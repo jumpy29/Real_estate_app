c0c14cde440b83a07276ccd52b1440c9fcf9a779
Smit Amin <smitamin@uvic.ca>
2025-06-27 10:19:07 +0530

    added views and displaying records

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,8 @@
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/estate_property_views.xml'
+        'views/estate_property_views.xml',
+        'views/estate_property_menu.xml'
     ],
     'application': True
 }
--- models/estate_properties.py
+++ models/estate_properties.py
@@ -1,4 +1,5 @@
 from odoo import models, fields
+from datetime import date, timedelta
 
 class EstateProperties(models.Model):
     _name = 'estate.properties'  #name of table
@@ -8,13 +9,25 @@ class EstateProperties(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default=date.today()+timedelta(days=90), copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection([('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')])
+    active = fields.Boolean(default=True)
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('cancelled', 'Cancelled')
+    ], 
+    required=True, 
+    copy=False,
+    default='new'
+    )
