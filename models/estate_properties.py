887235956c491565b9d8bc61a2186d171055c72b
Smit Amin <smitamin@uvic.ca>
2025-07-03 13:16:53 +0530

    created property types and tags and added them to views

--- models/estate_properties.py
+++ models/estate_properties.py
@@ -31,3 +31,7 @@ class EstateProperties(models.Model):
     copy=False,
     default='new'
     )
+    property_type_id = fields.Many2one('estate.property.type', string='Property Type')
+    salesperson_id = fields.Many2one('res.users', string='Salesperson', default=lambda self: self.env.user)
+    buyer_id = fields.Many2one("res.partner", string='Buyer', copy = False)
+    property_tag_ids = fields.Many2many('estate.property.tag', string='Tags')
